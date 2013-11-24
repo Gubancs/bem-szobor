6cf47c7eda7ead922347e353302061bea0daf87b
Istvan Benedek <istvan.benedek@bestest.hu>
2013-11-24 18:29:13 +0100

    improved

--- net.topclouders.bem-szobor.web/src/main/webapp/js/protest.js
+++ net.topclouders.bem-szobor.web/src/main/webapp/js/protest.js
@@ -1,162 +1,173 @@
-		google.load('visualization', '1', {
-			'packages' : [ 'geomap' ]
-		});
-		google.setOnLoadCallback(drawMarkersMap);
+var SERVER_URL = "http://localhost:8080/bemszobor";
 
-		function drawMarkersMap() {
-			var data = google.visualization.arrayToDataTable([
-					[ 'Country', 'Valami' ], [ 'England', 2761477 ],
-					[ 'Hungary', 2761477 ], [ 'Austria', 959574 ],
-					[ 'Germany', 907563 ], [ 'Finnland', 655875 ],
-					[ 'Brazil', 655875 ], [ 'Croatia', 655875 ] ]);
+var test = {
+	"V" : 100000,
+	"CD" : 56000,
+	"D" : 200000
+};
 
-			var options = {
-				language : "hu",
-				displayMode : 'region',
-				colorAxis : {
-					colors : [ 'yellow', 'red' ]
-				}
+var test2 = [ {
+	"age" : 18,
+	"f" : 2,
+	"m" : 0
+}, {
+	"age" : 19,
+	"f" : 0,
+	"m" : 2
+}, {
+	"age" : 30,
+	"f" : 1,
+	"m" : 4
+}, {
+	"age" : 32,
+	"f" : 0,
+	"m" : 1
+} ];
+
+function getDemonstrationId() {
+	return 3;// TODO modify
+}
+
+// Load the Visualization API and the piechart package.
+google.load('visualization', '1', {
+	'packages' : [ 'corechart' ]
+});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.setOnLoadCallback(updatechartsonFirstTab);
+
+function getTotalData() {
+	return test;// TODO modify
+
+	/*
+	 * var serviceUrl = SERVER_URL +"/stat/total/"+getDemonstrationId();
+	 * 
+	 * $.getJSON(serviceUrl,function(result){ updatetotalSection(result[0]);
+	 * 
+	 * });
+	 * 
+	 */
+}
+
+function getData4FirstTab() {
+	return test2;// TODO modify
+
+	/*
+	 * var serviceUrl = SERVER_URL +"/stat/total/"+getDemonstrationId();
+	 * 
+	 * $.getJSON(serviceUrl,function(result){ updatetotalSection(result[0]);
+	 * 
+	 * });
+	 * 
+	 */
+}
+var total_ageChart;
+var total_genderChart;
+
+function updatechartsonFirstTab() {
+	
+	total_ageChart = new google.visualization.AreaChart(document
+			.getElementById('total_areachart_age'));
+	
+	total_genderChart = new google.visualization.PieChart(document
+			.getElementById('total_piechart_gender'));
+
+	var result = getData4FirstTab();
+	drawAgeChart(result, total_ageChart);
+	drawGenderChart(result, total_genderChart);
+}
+
+
+function drawGenderChart(list, chart) {
+	var female = 0;
+	var male = 0;
+	for (var i = 0; i < list.length; i++) {
+		female = female + list[i]["f"];
+		male = male + list[i]["m"];
+	}
+
+	var data = google.visualization.arrayToDataTable([ [ 'Nem', 'Fő' ],
+			[ 'Férfi', male ], [ 'Nő', female ] ]);
+
+	var options = {
+		fontName : 'Helvetica',
+		is3D : true,
+		colors : [ '#428bca', '#dc3912' ]
+	};
+
+	chart.draw(data, options);
+}
+
+
+
+function drawAgeChart(list, chart) {
+	var data = new google.visualization.DataTable();
+	data.addColumn('number', 'Életkor');
+	data.addColumn('number', 'Férfi');
+	data.addColumn('number', 'Nő');
+	for (var i = 0; i < list.length; i++) {
+		data.addRow([ list[i]["age"], list[i]["m"], list[i]["f"] ]);
+	}
+
+	var options = {
+		hAxis : {
+			title : 'Életkor',
+			titleTextStyle : {
+				color : '#5a5a5a'
+			}
+		},
+		vAxis : {
+			minValue : 0
+		},
+
+		fontName : 'Helvetica',
+		colors : [ '#428bca', '#dc3912' ]
+
+	};
+
+	chart.draw(data, options);
+}
+
+function updadateTotalTab(){
+	updatetotalSection(getTotalData());
+	var data = getData4FirstTab();
+	drawAgeChart(data, total_ageChart);
+	drawGenderChart(data, total_genderChart);
+}
+
+function updatetotalSection(value) {
+
+	var no_of_counterprotesters = value["CD"];
+	var no_of_protesters = value["D"];
+	var no_of_visitors = value["V"];
+	var total = no_of_counterprotesters + no_of_protesters + no_of_visitors;
+
+	$("#no_protesters").text(no_of_protesters);
+	$("#no_counterprotesters").text(no_of_counterprotesters);
+	$("#no_visitors").text(no_of_visitors);
+	$("#no_total").text(total);
+}
+
+var c = 33;
+function start() {
+	updatetotalSection(getTotalData());
+	setInterval(function(){
+		updadateTotalTab();
+		c = c+1;
+		test2.push({"age" : c,
+		        	"f" : 10,
+		        	"m" : 13});
+		
+		test = {
+				"V" : test["V"]+1000,
+				"CD" :  test["CD"]+1000,
+				"D" : test["D"]+2000
 			};
+	}, 3000);
+}
 
-			var chart = new google.visualization.GeoChart(document
-					.getElementById('chart_div'));
-			chart.draw(data, options);
+start();
 
-			google.visualization.events.addListener(chart, 'regionClick',
-					function() {
-						var selection = chart.getSelection();
+$(document).ready(function() {
 
-						var data = google.visualization
-								.arrayToDataTable([ [ 'City', 'Valami' ],
-										[ 'Budapest', 2761477 ],
-										[ 'Debrecen', 2761477 ],
-										[ 'Esztergom', 959574 ],
-										[ 'Békéscsaba', 907563 ],
-										[ 'Körmend', 655875 ],
-										[ 'Komló', 655875 ],
-										[ 'Zalaegerszeg', 655875 ],
-										[ 'Pilismarót', 345345 ],
-										[ 'Borota', 2345345 ] ]);
-
-						var options = {
-							language : "hu",
-							region : 'HU',
-							displayMode : 'markers',
-							colorAxis : {
-								colors : [ 'yellow', 'red' ]
-							}
-						};
-
-						chart.draw(data, options);
-
-					});
-
-		};
-
-
-      google.load("visualization", "1", {packages:["corechart"]});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Életkor', 'Férfi', 'Nő'],
-          ['18',  100,      45],
-          ['19',  120,      400],
-          ['20',  130,      400],
-          ['21',  140,      400],
-          ['22',  180,      400],
-          ['23',  200,      400],
-          ['24',  240,      400],
-          ['25',  250,      400],
-          ['26',  250,      400],
-          ['27',  250,      400],
-          ['28',  210,      400],
-          ['29',  300,      400],
-          ['30',  310,      400],
-          ['31',  350,      400],
-          ['32',  310,      400],
-          ['33',  350,      400],
-          ['34',  400,      400],
-          ['35',  460,      400],
-          ['36',  450,      400],
-          ['37',  500,      400],
-          ['38',  550,      400],
-          ['39',  463,      400],
-          ['40',  444,      400],
-          ['41',  450,      400],
-          ['42',  430,      400],
-          ['43',  400,      400],
-          ['44',  250,      400],
-          ['45',  200,      400],
-          ['46',  260,      400],
-          ['47',  200,      400],
-          ['48',  160,      400],
-          ['49',  130,      400],
-          ['50',  120,      400],
-          ['51',  110,      400],
-          ['52',  100,      400],
-          ['53',  50,      400],
-          ['54',  50,      400],
-          ['55',  50,      400],
-          ['56',  50,      400],
-          ['57',  50,      400],
-          ['58',  70,      400],
-          ['59',  60,      400],
-          ['60',  50,      400]
-          
-        ]);
-
-        var options = {
-          hAxis: {title: 'Életkor', titleTextStyle: {color: '#5a5a5a'}},
-          vAxis: {minValue: 0},
-
-          fontName: 'Helvetica',
-          colors: ['#428bca','#dc3912']
-          
-        };
-
-        var chart = new google.visualization.AreaChart(document.getElementById('agechartstat_div'));
-        chart.draw(data, options);
-        
-        var chart = new google.visualization.AreaChart(document.getElementById('agechartstat_div2'));
-        chart.draw(data, options);
-      }
-
-      google.load("visualization", "1", {packages:["corechart"]});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Gender', 'Number'],
-          ['Férfi',   234234],
-          ['Nő',      182342]
-        ]);
-
-        var options = {
-          fontName: 'Helvetica',
-          is3D: true,
-          colors: ['#428bca','#dc3912']
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-        chart.draw(data, options);
-      }
-
-     google.load("visualization", "1", {packages:["corechart"]});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Type', 'number'],
-          ['Tüntetők',     154000],
-          ['Ellentüntetők',      24000],
-          ['Nézelődők',  13000]
-        ]);
-
-        var options = {
-
-          fontName: 'Helvetica',
-          pieHole: 0.4
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-        chart.draw(data, options);
-      }
- 
+});
